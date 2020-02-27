33ac55be5ac13f167f8a85a91dea528096a68e8b
SouichirouTakahashi2 <ar.hickers@gmail.com>
2020-02-27 11:36:44 +0900

    L6　の課題である「通り過ぎて画面外にでたアイテムを直ちに廃棄する」　をしました。よろしくお願いします

---
+++ Assets/DestroyObjectController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectController : MonoBehaviour {
+
+    Renderer objectRenderer;
+
+    // Use this for initialization
+    void Start () {
+
+        objectRenderer = GetComponent<Renderer>();
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (objectRenderer.isVisible)
+            {
+                 Destroy(gameObject);
+            }
+
+
+    }
+}
